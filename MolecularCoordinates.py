1dc4519d1647e8ef694c07a80c53fd8f3f8d4a5e
Gregory Magoon <gmagoon@mit.edu>
2010-02-11 11:55:45 -0500

    added distance mappings function

--- MolecularCoordinates.py
+++ MolecularCoordinates.py
@@ -68,6 +68,45 @@ class MolecularGeometry:
 			dist[j][i]=dist[i][j]
 
 		return dist
+
+	def getDistanceMappings(self):
+		"""
+		Generates dictionaries mapping atom label pairs to associated distances
+
+		Outputs are heterogeneous mappings (different atom types in tuple) and homogeneous mappings (same atom types) to distances and mappings to atomTypeTuples
+		For heterogeneous mappings, the atom type tuple will be ordered with lowest # atom type first
+
+		Example:
+		>>> import MolecularCoordinates
+		>>> a = MolecularCoordinates.MolecularGeometry([6,1,1,1],[[0,0,0],[1,0,0],[0,1,0],[0,0,1]])
+		>>> b = a.getDistanceMappings()
+		>>> b
+		({(3, 1): 1.0, (4, 1): 1.0, (2, 1): 1.0}, {(3, 4): 1.4142135623730951, (2, 3): 1.4142135623730951, (2, 4): 1.4142135623730951}, {(3, 1): (1, 6), (4, 1): (1, 6), (2, 1): (1, 6)}, {(3, 4): (1, 1), (2, 3): (1, 1), (2, 4): (1, 1)})
+		"""
+		dist = self.getDistanceMatrix()
+		natoms = self.atoms
+		hetMap = {}
+		homMap = {}
+		hetMapType={}
+		homMapType={}
+		for i in range(0,natoms):
+		    atomType1 = self.atomTypeDict[i+1]
+		    for j in range(i+1, natoms):
+			atomType2 = self.atomTypeDict[j+1]
+			if (atomType1 == atomType2): #if the atom types are the same, put the distance in homMap
+			    homMap[(i+1,j+1)]=dist[i][j]
+			    homMapType[(i+1,j+1)]=(atomType1,atomType2)
+			elif (atomType1 < atomType2):#otherwise, put the distance in hetMap, with order of tuple determined by lowest # atom type
+			    hetMap[(i+1,j+1)]=dist[i][j]
+			    hetMapType[(i+1,j+1)]=(atomType1,atomType2)
+			else:
+			    hetMap[(j+1,i+1)]=dist[i][j]
+			    hetMapType[(j+1,i+1)]=(atomType2,atomType1)
+
+		return hetMap, homMap, hetMapType, homMapType
+
+
+
 ################################################################################
 def checkConformationalEquivalence(mg1, mg2, Atol=-1, Rtol=-1):
 	"""Checks conformational equivalence between two molecules within a user specified tolerance
@@ -81,8 +120,8 @@ def checkConformationalEquivalence(mg1, mg2, Atol=-1, Rtol=-1):
 	#assert mg1 and mg2 contain the same number of each type of atom (and obviously the same total number of atoms)
 	#assert Atol > 0 or Rtol > 0 (and maybe also that Atol < 0 or Rtol < 0; i.e. only one of Atol and Rtol should be specified)
 
-	#generate distance matrices
-	dist1=mg1.getDistanceMatrix()
-	dist2=mg2.getDistanceMatrix()
+	#generate distance mappings
+	(hetMap, homMap, hetMapType, homMapType)=mg1.getDistanceMappings()
+	(hetMap, homMap, hetMapType, homMapType)=mg2.getDistanceMappings()
 
 	return matchQ, nmatches
