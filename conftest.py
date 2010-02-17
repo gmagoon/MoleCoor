6684f0d80f1273f09990575e4674b9cb778049bc
Gregory Magoon <gmagoon@mit.edu>
2010-02-17 14:41:18 -0500

    fixed indentation

--- conftest.py
+++ conftest.py
@@ -4,14 +4,15 @@ import MolecularCoordinates
 
 class  ConfTestCase(unittest.TestCase):
 
-    def simpleConfTest(self):
-	a = MolecularCoordinates.MolecularGeometry([6,1,1,1],[[0,0,0],[1,0,0],[0,1,0],[0,0,1]])
-	b = MolecularCoordinates.MolecularGeometry([6,1,1,1],[[0,0,0],[1,0,0],[0,1,0],[0,0,1]])
-	q = MolecularCoordinates.checkConformationalEquivalence(b, a, Atol=0.01)
-	print q
-        #assert x != y;
-        #self.assertEqual(x, y, "Msg");
+	def simpleConfTest(self):
+		a = MolecularCoordinates.MolecularGeometry([6,1,1,1],[[0,0,0],[1,0,0],[0,1,0],[0,0,1]])
+		b = MolecularCoordinates.MolecularGeometry([6,1,1,1],[[0,0,0],[1,0,0],[0,1,0],[0,0,1]])
+		q = MolecularCoordinates.checkConformationalEquivalence(b, a, Atol=0.01)
+		print q
+		#assert x != y;
+		#self.assertEqual(x, y, "Msg");
+		self.fail("TODO: Write test")
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
 
