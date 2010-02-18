67ead1d0b5818daaba849e837f2e2d159f4d4180
Gregory Magoon <gmagoon@mit.edu>
2010-02-17 19:34:55 -0500

    additional documentation on unit tests

--- conftest.py
+++ conftest.py
@@ -7,6 +7,7 @@ class  ConfTestCase(unittest.TestCase):
 	def testSimpleHetConf(self):
 		""" A simple test (using the same molecule) of the heterogeneous case
 
+		A pyramidal form for CH3
 		"""
 		a = MolecularCoordinates.MolecularGeometry([6,1,1,1],[[0,0,0],[1,0,0],[0,1,0],[0,0,1]])
 		b = MolecularCoordinates.MolecularGeometry([6,1,1,1],[[0,0,0],[1,0,0],[0,1,0],[0,0,1]])
@@ -17,6 +18,7 @@ class  ConfTestCase(unittest.TestCase):
 	def testSimpleHomConf(self):
 		""" A simple test (using the same molecule) of the homogenous case
 
+		A cube of hydrogens
 		"""
 		a = MolecularCoordinates.MolecularGeometry([1,1,1,1,1,1,1,1],[[0,0,0],[1,0,0],[0,1,0],[0,0,1],[0,1,1],[1,0,1],[1,1,0],[1,1,1]])
 		b = MolecularCoordinates.MolecularGeometry([1,1,1,1,1,1,1,1],[[0,0,0],[1,0,0],[0,1,0],[0,0,1],[0,1,1],[1,0,1],[1,1,0],[1,1,1]])
