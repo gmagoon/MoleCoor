363a8105d61709c22a2277813c753abd3836d5db
Gregory Magoon <gmagoon@mit.edu>
2010-02-20 23:35:07 -0500

    reordered "if" check to try to speed up
    May provide a very slight speedup:
    test1 took 0.254 seconds ([0.30161682560213654, 0.26111866172935372, 0.25357908616877278, 0.25848634393477354, 0.25954842026011704])
    test2 took 0.099 seconds ([0.10295421624815448, 0.1012709715899649, 0.10328372740110847, 0.099218825297628932, 0.099519841208868698])
    test3 took 0.211 seconds ([0.21692463706979481, 0.21613165919132182, 0.21106900458019062, 0.21891350716362012, 0.21080318867342029])
    
    Continuing with tests...
    A test of distance deviations, given a mapping ... ok
    A test of mirror images ... ok
    A simple test (using the same molecule) of the heterogeneous case ... ok
    A simple test (using the same molecule) of the homogenous case ... ok
    
    ----------------------------------------------------------------------
    Ran 4 tests in 0.016s
    
    OK

--- MolecularCoordinates.py
+++ MolecularCoordinates.py
@@ -161,7 +161,7 @@ def checkDistance(hetMap1, homMap1, hetMapType1, homMapType1, hetMap2, homMap2, 
 		mappingType = hetMapType1.pop(mappingLabels)
 		hetMapType2_icopy = hetMapType2.copy()#make a copy of hetMapType2 for the purposes of iteration (according to Python docs, iterating while adding or removing entries may cause RuntimeError)
 		for i in hetMapType2_icopy:#search in hetMapType2 for cases with the same mapping type and a pre-established atom correspondence
-			if(hetMap2TargetLabel==-1 or hetMap2TargetLabel==i[0] or hetMap2TargetLabel==i[1]): # this line checks whether the target atom label is consistent with previously established mappings;
+			if(hetMap2TargetLabel==i[0] or hetMap2TargetLabel==i[1] or hetMap2TargetLabel==-1): # this line checks whether the target atom label is consistent with previously established mappings;
 				if(hetMapType2[i]==mappingType):#this line does the mapping type check
 					if(distanceMatchQ(mapping[1],hetMap2[i], Atol=Atol, Rtol=Rtol)): #for each case where this is true, check that all other het and hom mappings involving already identified atoms also satisfy the constriant, removing them in the process
 						#copy all the dictionaries
@@ -209,7 +209,7 @@ def checkDistance(hetMap1, homMap1, hetMapType1, homMapType1, hetMap2, homMap2, 
 		mappingType = homMapType1.pop(mappingLabels)
 		homMapType2_icopy = homMapType2.copy()#make a copy of homMapType2 for the purposes of iteration (according to Python docs, iterating while adding or removing entries may cause RuntimeError)
 		for i in homMapType2_icopy:#search in homMapType2 for cases with the same mapping type;
-			if(homMap2TargetLabel==-1 or homMap2TargetLabel==i[0] or homMap2TargetLabel==i[1]): # this line checks whether the target atom label is consistent with previously established mappings
+			if(homMap2TargetLabel==i[0] or homMap2TargetLabel==i[1] or homMap2TargetLabel==-1): # this line checks whether the target atom label is consistent with previously established mappings
 				if(homMapType2[i]==mappingType):
 					if(distanceMatchQ(mapping[1],homMap2[i], Atol=Atol, Rtol=Rtol)): #for each case where this returns true, check that all other het and hom mappings involving already identified atoms also satisfy the constriant, removing them in the process
 						#copy all the dictionaries
